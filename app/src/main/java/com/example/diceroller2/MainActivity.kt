98c4ba4619106dd8c671df90f369a305f0d8dbc8
pawelmorozewicz <p.morozewicz99@gmail.com>
2023-11-11 21:53:18 +0100

    optimize function RollDice and made blank screen before rolling in app

--- app/src/main/java/com/example/diceroller2/MainActivity.kt
+++ app/src/main/java/com/example/diceroller2/MainActivity.kt
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,12 +18,13 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener{
             rollDice()
         }
+
+        diceImage = findViewById(R.id.imageView)
     }
 
     private fun rollDice() {
 
-        val randomInt = Random.nextInt(6) + 1
-        val DrawableResource = when (randomInt) {
+        val drawableResource = when (Random.nextInt(6) + 1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -30,9 +32,8 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        val diceImage: ImageView = findViewById(R.id.imageView)
 
-        diceImage.setImageResource(DrawableResource)
+        diceImage.setImageResource(drawableResource)
 
     }
 
